0c9c52139e63f66e595ba8ca2f72b14e45d85408
Amanda Dutra <amanda.dutra@montreal.com.br>
2024-09-20 10:14:44 -0300

    Refatoração do código "EvolucaoTestes.IRPF"

--- MB.Console.PPA.EvolucaoTestes.IRPF/Program.cs
+++ MB.Console.PPA.EvolucaoTestes.IRPF/Program.cs
@@ -1,85 +1,98 @@
-﻿using System;
-
-namespace CalculaImposto;
-class Program
+﻿namespace EvolucaoTestes.IRPF
 {
-    static void Main()
+    class Program
     {
-        int numeroContribuintes;
-        while (true)
+        static void Main(string[] args)
+        {
+            int numeroContribuintes = validarDadosEntrada();
+            processarDadosContribuintes(numeroContribuintes);
+        }
+
+        static int validarDadosEntrada()
         {
             Console.Write("Informe o número de contribuintes a calcular: ");
-            if (int.TryParse(Console.ReadLine(), out numeroContribuintes) && numeroContribuintes > 0)
+            if (int.TryParse(Console.ReadLine(), out int numeroContribuinte) && numeroContribuinte > 0)
             {
-                break;
+                return numeroContribuinte;
             }
             else
             {
                 Console.WriteLine("Por favor, informe um valor válido para o número de contribuintes.");
+                return validarDadosEntrada();
             }
         }
 
-        for (int i = 0; i < numeroContribuintes; i++)
+        static void processarDadosContribuintes(int numeroContribuintes)
         {
-            Console.WriteLine($"\nContribuinte {i + 1}");
-            Console.WriteLine("Informe o nome do contribuinte: ");
-            string nomeContribuinte = Console.ReadLine() ?? "Desconhecido";
+            for (int i = 0; i < numeroContribuintes; i++)
+            {
+                Console.WriteLine($"\nContribuinte {i + 1}");
+                Console.Write("Informe o nome do contribuinte: ");
+                string nomeContribuinte = Console.ReadLine() ?? "Desconhecido";
 
-            decimal salarioBruto;
-            while (true)
+                decimal salarioBruto = validarSalarioEntrada();
+
+                decimal desconto = calcularDesconto(salarioBruto);
+                decimal salarioLiquido = salarioBruto - desconto;
+
+                exibirResultados(nomeContribuinte, salarioBruto, desconto, salarioLiquido);
+            }
+        }
+
+        static decimal validarSalarioEntrada()
+        {
+            Console.Write("Informe um valor válido para salário: ");
+            if (decimal.TryParse(Console.ReadLine(), out decimal salarioBruto) && salarioBruto >= 0)
             {
-                Console.Write("Informe um valor válido para salário: ");
-                if (decimal.TryParse(Console.ReadLine(), out salarioBruto) && salarioBruto >= 0)
-            {
-                break;
+                return salarioBruto;
             }
             else
             {
                 Console.WriteLine("Por favor, informe um valor válido para salário.");
+                return validarSalarioEntrada();
             }
+        }
+
+        static decimal calcularDesconto(decimal salarioBruto)
+        {
+            decimal aliquota;
+            decimal deducao;
+
+            if (salarioBruto <= 1903.99m)
+            {
+                aliquota = 0m;
+                deducao = 0m;
             }
-                decimal desconto = CalcularDesconto(salarioBruto);
-                decimal salarioLiquido = salarioBruto - desconto;
+            else if (salarioBruto <= 2826.65m)
+            {
+                aliquota = 0.075m;
+                deducao = 142.80m;
+            }
+            else if (salarioBruto <= 3751.05m)
+            {
+                aliquota = 0.15m;
+                deducao = 354.80m;
+            }
+            else if (salarioBruto <= 4664.68m)
+            {
+                aliquota = 0.225m;
+                deducao = 636.13m;
+            }
+            else
+            {
+                aliquota = 0.275m;
+                deducao = 869.36m;
+            }
 
-                Console.WriteLine($"Nome do Contribuinte: {nomeContribuinte}");
-                Console.WriteLine($"Salário Bruto: R$ {salarioBruto:F2}");
-                Console.WriteLine($"Desconto: R$ {desconto:F2}");
-                Console.WriteLine($"Salário Líquido: R$ {salarioLiquido:F2}");
-        }
-    }
-
-    static decimal CalcularDesconto(decimal salarioBruto)
-    {
-        decimal aliquota;
-        decimal deducao;
-
-        if (salarioBruto <= 1903.99m)
-        {
-            aliquota = 0m;
-            deducao = 0m;
-        }
-        else if (salarioBruto <= 2826.65m)
-        {
-            aliquota = 0.075m;
-            deducao = 142.80m;
-        }
-        else if (salarioBruto <= 3751.05m)
-        {
-            aliquota = 0.15m;
-            deducao = 354.80m;
-        }
-        else if (salarioBruto <= 4664.68m)
-        {
-            aliquota = 0.225m;
-            deducao = 636.13m;
-        }
-        else
-        {
-            aliquota = 0.275m;
-            deducao = 869.36m;
+            return salarioBruto * aliquota - deducao;
         }
 
-        decimal desconto = salarioBruto * aliquota - deducao;
-        return desconto;
+        static void exibirResultados(string nome, decimal salarioBruto, decimal desconto, decimal salarioLiquido)
+        {
+            Console.WriteLine($"Nome do Contribuinte: {nome}");
+            Console.WriteLine($"Salário Bruto: R$ {salarioBruto:F2}");
+            Console.WriteLine($"Desconto: R$ {desconto:F2}");
+            Console.WriteLine($"Salário Líquido: R$ {salarioLiquido:F2}");
+        }
     }
 }
