2e37214d51fb450d04e02c854c5cc0dfaf15efe8
Amanda Dutra <amanda.dutra@montreal.com.br>
2024-09-20 10:44:37 -0300

    Refatoração da organização de pastas

--- Program.cs
+++ Program.cs
@@ -1,70 +1,59 @@
-﻿using System;
-
-namespace EvolucaoTestes.IRPF
+﻿namespace EvolucaoTestes.IRPF
 {
     class Program
     {
-          static void Main(string[] args)
+        static void Main(string[] args)
         {
-            int numeroContribuintes = ValidarDadosEntrada();
-            ProcessarDadosContribuintes(numeroContribuintes);
+            int numeroContribuintes = validarDadosEntrada();
+            processarDadosContribuintes(numeroContribuintes);
         }
 
-        static int ValidarDadosEntrada()
+        static int validarDadosEntrada()
         {
-            int numeroContribuinte;
-            for (;;)
+            Console.Write("Informe o número de contribuintes a calcular: ");
+            if (int.TryParse(Console.ReadLine(), out int numeroContribuinte) && numeroContribuinte > 0)
             {
-                Console.Write("Informe o número de contribuintes a calcular: ");
-                if (int.TryParse(Console.ReadLine(), out numeroContribuinte) && numeroContribuinte > 0)
-                {
-                    return numeroContribuinte;
-                }
-                else
-                {
-                    Console.WriteLine("Por favor, informe um valor válido para o número de contribuintes.");
-                }
+                return numeroContribuinte;
+            }
+            else
+            {
+                Console.WriteLine("Por favor, informe um valor válido para o número de contribuintes.");
+                return validarDadosEntrada();
             }
         }
 
-        static void ProcessarDadosContribuintes(int numeroContribuintes)
+        static void processarDadosContribuintes(int numeroContribuintes)
         {
             for (int i = 0; i < numeroContribuintes; i++)
             {
                 Console.WriteLine($"\nContribuinte {i + 1}");
-                Console.WriteLine("Informe o nome do contribuinte: ");
+                Console.Write("Informe o nome do contribuinte: ");
                 string nomeContribuinte = Console.ReadLine() ?? "Desconhecido";
 
-                decimal salarioBruto = ValidarSalarioEntrada();
+                decimal salarioBruto = validarSalarioEntrada();
 
-                decimal desconto = CalcularDesconto(salarioBruto);
+                decimal desconto = calcularDesconto(salarioBruto);
                 decimal salarioLiquido = salarioBruto - desconto;
 
-                Console.WriteLine($"Nome do Contribuinte: {nomeContribuinte}");
-                Console.WriteLine($"Salário Bruto: R$ {salarioBruto:F2}");
-                Console.WriteLine($"Desconto: R$ {desconto:F2}");
-                Console.WriteLine($"Salário Líquido: R$ {salarioLiquido:F2}");
+                exibirResultados(nomeContribuinte, salarioBruto, desconto, salarioLiquido);
             }
         }
 
-        static decimal ValidarSalarioEntrada()
+        static decimal validarSalarioEntrada()
         {
-            decimal salarioBruto;
-            for (;;)
+            Console.Write("Informe um valor válido para salário: ");
+            if (decimal.TryParse(Console.ReadLine(), out decimal salarioBruto) && salarioBruto >= 0)
             {
-                Console.Write("Informe um valor válido para salário: ");
-                if (decimal.TryParse(Console.ReadLine(), out salarioBruto) && salarioBruto >= 0)
-                {
-                    return salarioBruto;
-                }
-                else
-                {
-                    Console.WriteLine("Por favor, informe um valor válido para salário.");
-                }
+                return salarioBruto;
+            }
+            else
+            {
+                Console.WriteLine("Por favor, informe um valor válido para salário.");
+                return validarSalarioEntrada(); 
             }
         }
 
-        static decimal CalcularDesconto(decimal salarioBruto)
+        static decimal calcularDesconto(decimal salarioBruto)
         {
             decimal aliquota;
             decimal deducao;
@@ -95,8 +84,15 @@ namespace EvolucaoTestes.IRPF
                 deducao = 869.36m;
             }
 
-            decimal desconto = salarioBruto * aliquota - deducao;
-            return desconto;
+            return salarioBruto * aliquota - deducao;
+        }
+
+        static void exibirResultados(string nome, decimal salarioBruto, decimal desconto, decimal salarioLiquido)
+        {
+            Console.WriteLine($"Nome do Contribuinte: {nome}");
+            Console.WriteLine($"Salário Bruto: R$ {salarioBruto:F2}");
+            Console.WriteLine($"Desconto: R$ {desconto:F2}");
+            Console.WriteLine($"Salário Líquido: R$ {salarioLiquido:F2}");
         }
     }
 }
