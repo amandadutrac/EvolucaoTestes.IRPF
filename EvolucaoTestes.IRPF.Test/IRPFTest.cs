7d68760f564a890ffeacdd454e42e934ea014f27
Amanda Dutra <amanda.dutra@montreal.com.br>
2024-10-07 14:00:20 -0300

    Testes IRPF

--- EvolucaoTestes.IRPF.Test/IRPFTest.cs
+++ EvolucaoTestes.IRPF.Test/IRPFTest.cs
@@ -1,9 +1,27 @@
-﻿namespace EvolucaoTestes.Tests
+﻿
+namespace EvolucaoTestes.IRPF.Tests
 {
-    internal class IRPFTest
+    public class IRPFTest
     {
-        public IRPFTest()
+        [Theory(DisplayName = "Deve calcular desconto correto")]
+        [InlineData(1903.99, 0)]
+        [InlineData(2826.65, 142.80)]
+        [InlineData(3751.05, 354.80)]
+        [InlineData(4664.68, 636.13)]
+        public void CalcularImposto_SucessTest(decimal salarioBruto, decimal descontoEsperado)
         {
+            decimal descontoCalculado = IRPF.CalcularDesconto(salarioBruto);
+
+            Assert.Equal(descontoEsperado, descontoCalculado);
+        }
+
+        [Fact(DisplayName = "Deve falhar ao calcular o desconto incorreto")]
+        public void CalcularImposto_FailTest()
+        {
+            decimal salarioBruto = 2826.65m;
+            decimal descontoCalculado = IRPF.CalcularDesconto(salarioBruto);
+
+            Assert.NotEqual(100.80m, descontoCalculado);
         }
     }
 }
