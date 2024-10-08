a578e3c99916ea1f5d6e3f73bdcbb8c49d353ff3
Amanda Dutra <amanda.dutra@montreal.com.br>
2024-10-08 11:05:29 -0300

    Remover pastas bin, obj e .vs do controle de versão

--- EvolucaoTestes.IRPF.Test/IRPFTest.cs
+++ EvolucaoTestes.IRPF.Test/IRPFTest.cs
@@ -5,9 +5,9 @@ namespace EvolucaoTestes.IRPF.Tests
     {
         [Theory(DisplayName = "Deve calcular desconto correto")]
         [InlineData(1903.99, 0)]
-        [InlineData(2826.65, 142.80)]
-        [InlineData(3751.05, 354.80)]
-        [InlineData(4664.68, 636.13)]
+        [InlineData(2826.65, 69.19875)]
+        [InlineData(3751.05, 207.8575)]
+        [InlineData(4664.68, 413.42300)]
         public void CalcularImposto_SucessTest(decimal salarioBruto, decimal descontoEsperado)
         {
             decimal descontoCalculado = IRPF.CalcularDesconto(salarioBruto);
